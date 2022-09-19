9980086018c9ff547a4401f3260ac9dc4813eb6a
Abass <abass_gbolahan@yahoo.com>
2022-09-19 08:32:24 +0100

    useState added to get meme images

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,14 +1,26 @@
 import React from "react";
+import memesData from "../memesData";
 
 
 function Meme() {
+
+    const [memeImage, setMemeImage] = React.useState("")
+    function getMemeImage() {
+        // console.log("image clicked");
+        const memesArray = memesData.data.memes
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        // const url = memesArray[randomNumber].url
+        setMemeImage( memesArray[randomNumber].url)
+
+    }
     return (
         <main>
-        <form className="form">
+        <div className="form">
             <input type="text" placeholder="Top Text" className="form--input" />
             <input type="text" placeholder="Bottom Text" className="form--input" />
-            <button className="form--button">Get a new meme image 🖼</button>
-        </form>
+            <button onClick={getMemeImage} className="form--button">Get a new meme image 🖼</button>
+        </div>
+        <img src={memeImage} className="meme--image"></img>
     </main>
     )
 }
